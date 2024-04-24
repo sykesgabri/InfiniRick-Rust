27846b2cb9176ab9ffca768adc65d910fe2ba10d
Gabriel Sykes <48891219+sykesgabri@users.noreply.github.com>
2024-04-24 09:33:53 +0100

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use webbrowser;
+
+fn main() {
+    let rick = "astley";
+
+    while rick == "astley" {
+
+        if webbrowser::open("https://www.youtube.com/watch?v=dQw4w9WgXcQ").is_ok() {
+
+        }
+
+    }
+
+}
